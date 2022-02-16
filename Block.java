dc604947d6ca12075a70e0acb6aa965fbe1a47dd
Jackson Schuiling <jacksonjschuiling@gmail.com>
2022-02-16 14:04:27 -0500

    feat: add block constructor

--- Block.java
+++ Block.java
@@ -2,20 +2,41 @@ import java.security.MessageDigest;
 import java.util.Date;
 
 public class Block{
-    public Block(int index, Date timestamp, String data, MessageDigest prevHash){
-        // TODO: Add code
+    private int index;
+    private Date timestamp;
+    private String data;
+    private MessageDigest prevHash;
+    private MessageDigest hash;
+
+    /**
+     * default constructor for a block
+     * @param index
+     * @param timestamp
+     * @param data
+     * @param prevHash
+     */
+    public Block(int index, Date timestamp, String data, MessageDigest prevHash) {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.prevHash = prevHash;
+        this.hash = hashblock();
     }
+
 
     private MessageDigest hashblock(){
         // TODO: Add code
         
     }
 
+
     public static  Block genesisBlock(){
         // TODO: Add code
     }
 
+
     public static Block newBlock(){
         // TODO: Add code
     }
+
 }
