8885a9fecfa887a5f6d3095cc6e99311720aed81
bbnumber2 <caleb.m.brinkman@gmail.com>
2022-02-16 13:55:39 -0500

    feat: Add skeleton for Block

---
+++ Block.java
@@ -0,0 +1,21 @@
+import java.security.MessageDigest;
+import java.util.Date;
+
+public class Block{
+    public Block(int index, Date timestamp, String data, MessageDigest prevHash){
+        // TODO: Add code
+    }
+
+    private MessageDigest hashblock(){
+        // TODO: Add code
+        
+    }
+
+    public static  Block genesisBlock(){
+        // TODO: Add code
+    }
+
+    public static Block newBlock(){
+        // TODO: Add code
+    }
+}
