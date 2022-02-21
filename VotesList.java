4c0db5ba2b886807e678cf874898d5433c13a6ed
Jackson Schuiling <jacksonjschuiling@gmail.com>
2022-02-20 20:58:37 -0500

    docs: add comments for the VotesList class

--- VotesList.java
+++ VotesList.java
@@ -3,11 +3,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class VotesList {
-    private ArrayList<String> votes;
-    private ArrayList<String> items;
+    private final ArrayList<String> votes;
+    private final ArrayList<String> items;
     private int numVotes;
     private int numItems;
 
+
+
+    // This is the constructor for the class. It initializes the votes and items lists, and sets the number of votes and
+    // items to 0.
     public VotesList(){
         votes = new ArrayList<String>();
         items = new ArrayList<String>();
@@ -15,10 +19,20 @@ public class VotesList {
         numItems = 0;
     }
 
+    /**
+     * Returns the list of votes
+     *
+     * @return An ArrayList of Strings.
+     */
     public ArrayList<String> getVotes(){
         return votes;
     }
 
+    /**
+     * Add an item to the ballot if it's not already on the ballot
+     *
+     * @param i the item to be added to the ballot.
+     */
     public void addItem(String i){
         if(!items.contains(i)) {
             items.add(i.toLowerCase());
@@ -29,6 +43,11 @@ public class VotesList {
         }
     }
 
+    /**
+     * Add the given vote to the list of votes
+     *
+     * @param v the item to be added to the ballot.
+     */
     public void addVote(String v){
         if(items.contains(v.toLowerCase())){
             votes.add(v);
@@ -39,14 +58,28 @@ public class VotesList {
         }
     }
 
+    /**
+     * Shuffle the votes array
+     */
     public void shuffleVotes(){
         Collections.shuffle(votes);
     }
 
+    /**
+     * Returns the items on the ballot
+     *
+     * @return An ArrayList of Strings.
+     */
     public ArrayList<String> getItems(){
         return items;
     }
 
+    /**
+     * Given a string, count the number of times that string appears in the list of votes
+     *
+     * @param v the name of the vote to count
+     * @return The number of votes for the given answer.
+     */
     public int countVotes(String v){
         int count = 0;
         for(String i : votes) {
@@ -57,24 +90,47 @@ public class VotesList {
         return count;
     }
 
+    /**
+     * Returns the total number of votes cast in the election
+     *
+     * @return The number of votes.
+     */
     public int getTotalVotes(){
         return numVotes;
     }
 
+    /**
+     * Returns the total number of items on the ballot
+     *
+     * @return The number of items on the ballot.
+     */
     public int getTotalItems(){
         return numItems;
     }
 
+    /**
+     * Remove the item from the ballot
+     *
+     * @param i the item to be removed
+     */
     public void removeItem(String i){
         items.remove(i);
         numItems--;
     }
 
+    /**
+     * Remove the last vote from the list of votes
+     */
     public void removeLastVote(){
         votes.remove(votes.size()-1);
         numVotes--;
     }
 
+    /**
+     * Remove all votes for a given item
+     *
+     * @param v the name of the vote to remove
+     */
     public void removeAllVotes(String v){
         for(String i : votes){
             if(i.equals(v)){
